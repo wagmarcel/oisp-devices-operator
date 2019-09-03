27747d13b19d5e464ba62ff77aa9d3946c9265a8
Marcel Wagner <wagmarcel@web.de>
2019-09-03 20:19:52 +0200

    fixed the role and role_bindings to allow listing and watching nodes

---
+++ pkg/apis/oisp/group.go
@@ -0,0 +1,6 @@
+// Package oisp contains oisp API versions.
+//
+// This file ensures Go source parsers acknowledge the oisp package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package oisp
